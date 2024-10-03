54b67d12866ba1175aecf7762f28f0b6038090f3
Roey Srugo <srugoroey@Roeys-MacBook-Pro.local>
2024-10-03 12:43:17 +0300

    basic app.js and index file

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("hellooooooooooo")
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const jwt = require("jsonwebtoken");
+
+// main
+const app = express();
+app.use(express.static("public"));
+
+app.use(express.json()); // to support JSON-encoded bodies
+app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
+app.set("view engine", "ejs");
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' });
+  });
+  
+const server = http.createServer(app);
+
+const run = async () => {
+    try {
+        const port = process.env.PORT;
+        server.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+run();
