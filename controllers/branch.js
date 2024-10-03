a8069f137f42c3a730e6998605d5525280c54de7
Noam Malka <noamalka2903@gmail.com>
2024-10-03 19:01:03 +0300

    Added get branch by id route

--- controllers/branch.js
+++ controllers/branch.js
@@ -1,7 +1,26 @@
-const { StatusCodes, CONFLICT } = require("http-status-codes");
+const { StatusCodes } = require("http-status-codes");
 const Branch = require("../models/branch");
 const BaseError = require("../errors");
+const mongoose = require("mongoose");
 // const {postTweet} = require('../twitter');
+
+async function getBranchById(req, res) {
+  try {
+    const branchId = req.params.id;
+    if (!mongoose.isValidObjectId(branchId)) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ error: 'Invalid branch ID format' });
+    }
+    const branch = await Branch.findById({_id: branchId});
+    if (branch) {
+        return res.status(StatusCodes.OK).json(branch);
+    }
+    return res.status(StatusCodes.NOT_FOUND).json(new BaseError.NotFoundError(`Object with id: ${branchId} not found!`));
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+}
 
 async function getAllBranches(_, res) {
   try {
@@ -21,23 +40,37 @@ async function createBranch(req, res) {
       address: req.body.address,
       city: req.body.city,
       phoneNumber: req.body.phoneNumber,
-      active: req.body.active
+      active: req.body.active,
     };
 
-    if (!branchData.name || !branchData.address || !branchData.city || !branchData.phoneNumber || !branchData.active) {
-        return res.status(StatusCodes.BAD_REQUEST).json("Not all required fields were provided!");
+    if (
+      !branchData.name ||
+      !branchData.address ||
+      !branchData.city ||
+      !branchData.phoneNumber ||
+      !branchData.active
+    ) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json("Not all required fields were provided!");
     }
     // TODO: change duplicate entry validations
-    if (await Branch.findOne({name: branchData.name})) {
-        return res.status(StatusCodes.CONFLICT).json("A branch with this name already exists!");
+    if (await Branch.findOne({ name: branchData.name })) {
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json("A branch with this name already exists!");
     }
-    if (await Branch.findOne({name: branchData.phoneNumber})) {
-        return res.status(StatusCodes.CONFLICT).json("A branch with this phone number already exists!");
+    if (await Branch.findOne({ name: branchData.phoneNumber })) {
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json("A branch with this phone number already exists!");
     }
-    if (await Branch.findOne({name: branchData.address})) {
-        return res.status(StatusCodes.CONFLICT).json("A branch with this address already exists!");
+    if (await Branch.findOne({ name: branchData.address })) {
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json("A branch with this address already exists!");
     }
-    
+
     const branch = new Branch({ ...branchData });
     Branch.create(branch);
     res.status(StatusCodes.OK).json({});
@@ -49,6 +82,7 @@ async function createBranch(req, res) {
 }
 
 module.exports = {
+  getBranchById,
   getAllBranches,
   createBranch,
 };
--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const branchController = require('../controllers/branch');
 
 // Route for creating a new branch
-router.get('/', branchController.getAllBranches)
+router.get('/:id', branchController.getBranchById);
+router.get('/', branchController.getAllBranches);
 router.post('/', branchController.createBranch);
 
 module.exports = router;
