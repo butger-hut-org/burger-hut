8dea6e7bfb7a016f8a8bfa664b096bb2ad6013f0
Roey Srugo <srugoroey@Roeys-MacBook-Pro.local>
2024-10-03 23:30:28 +0300

    add order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const middleware = require("../middleware/auth")
 const router = express.Router();
+const {
+    orderCreate,
+    orderDelete,
+    orderList
+} = require('../controllers/order');
 
+router.route('/create').post(middleware.verifyJwt, orderCreate);
+router.route('/delete').post(middleware.verifyJwt, orderDelete);
+router.route('/list').get(middleware.verifyJwt, orderList);
 module.exports = router;
