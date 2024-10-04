d6c6aa00f540164efe176fce289c8bc1ffa41f36
Roey Srugo <srugoroey@Roeys-MacBook-Pro.local>
2024-10-04 18:13:19 +0300

    minor change to order routes

--- routes/apiRoutes/orderRoutes.js
+++ routes/apiRoutes/orderRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const middleware = require("../middleware/auth")
+const middleware = require("../../middleware/auth")
 const router = express.Router();
 const {
     orderCreate,
     orderDelete,
     orderList
-} = require('../controllers/order');
+} = require('../../controllers/order');
 
 router.route('/create').post(middleware.verifyJwt, orderCreate);
 router.route('/delete').post(middleware.verifyJwt, orderDelete);
