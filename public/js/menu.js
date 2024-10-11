91703ce5d1728cbad71beb9d5e978b9fbb46f53c
EzPzLs <eyalzohar2@gmail.com>
2024-10-11 17:10:19 +0300

    items in array in local storage

--- public/js/menu.js
+++ public/js/menu.js
@@ -45,7 +45,7 @@ function generateMenuItems(products) {
         const price = document.createElement("p");
         price.textContent = `$${product.basePrice.toFixed(2)}`;
         const button = document.createElement("a");
-        
+
         button.textContent = "Add to Cart";
         button.classList.add("btn", "btn-primary");
         button.setAttribute('data-bs-toggle', 'modal');
@@ -69,9 +69,19 @@ function addItemIdToModal(id){
 }
 
 function addToTempCart(){
-    const itemFields = collectFormFields();
+    let itemFields = collectFormFields();
     const productId = document.getElementById('productId').innerText;
-    localStorage.setItem(productId, JSON.stringify(itemFields));
+    itemFields.productId = productId;
+    let currentProductsInCart = localStorage.getItem('cart');
+    console.log(currentProductsInCart);
+    if (!currentProductsInCart){
+        currentProductsInCart = [];
+    }
+    else {
+        currentProductsInCart = JSON.parse(currentProductsInCart);
+    }
+    currentProductsInCart.push(itemFields);
+    localStorage.setItem('cart', JSON.stringify(currentProductsInCart));
 }
 
 function collectFormFields() {
@@ -79,8 +89,6 @@ function collectFormFields() {
         size: $("#productSize").val(),
         quantity: $("#productQuantity").val()
     };
-    console.log(request.size);
-    console.log(request.quantity);
     if (request.size == null || request.quantity == null) {
         alert("Please fill out all fields");
         return null;
