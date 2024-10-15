6791015eae3634c258b37cfbf862ce2b5867848b
Noam Malka <noamalka2903@gmail.com>
2024-10-15 16:36:41 +0300

    Added conditional rendering of admin portal button in dropdown menu

--- routes/webRoutes/webRouter.js
+++ routes/webRoutes/webRouter.js
@@ -1,47 +1,43 @@
-const usersController = require('../../controllers/users');
+const usersController = require("../../controllers/users");
 const middleware = require("../../middleware/auth");
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 
-// Route for register page
 router.get("/register", async (req, res) => {
   res.render("public/register", {});
 });
 
-// Route for login page
 router.get("/login", async (req, res) => {
   res.render("public/login", {});
 });
 
-// Route for home page
 router.get("/home", middleware.verifyJwt, async (req, res) => {
-  res.render("./index");
+  res.render("./index", { isAdmin: await middleware.isAdmin(req) });
 });
 
-// Route for displaying branches
 router.get("/branches", middleware.verifyJwt, async (req, res) => {
-  res.render("public/branches");
+  res.render("public/branches", { isAdmin: await middleware.isAdmin(req) });
 });
 
-router.get('/menu', middleware.verifyJwt, async (req, res) => {
-  res.render('./menu');
+router.get("/menu", middleware.verifyJwt, async (req, res) => {
+  res.render("./menu", { isAdmin: await middleware.isAdmin(req) });
 })
 
 router.get("/adminPortal", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
-  res.render("./adminPortal.ejs");
+  res.render("./adminPortal.ejs", { isAdmin: await middleware.isAdmin(req) });
 });
 
 router.get("/branchManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
-  res.render("./branchMgmt");
+  res.render("./branchMgmt", { isAdmin: await middleware.isAdmin(req) });
 });
 
 router.get("/productManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
-  res.render("./productMgmt");
+  res.render("./productMgmt", { isAdmin: await middleware.isAdmin(req) });
 });
 
 router.get("/userManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
-  res.render("./userManagement.ejs");
+  res.render("./userManagement.ejs", { isAdmin: await middleware.isAdmin(req) });
 });
 
 router.get('/logout', usersController.logout);
