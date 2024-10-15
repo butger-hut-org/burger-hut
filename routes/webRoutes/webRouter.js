160897fc8a281fdd410a2fa528daf48414553317
Noam Malka <noamalka2903@gmail.com>
2024-10-15 15:40:20 +0300

    Added token and admin validation on all required routes

--- routes/webRoutes/webRouter.js
+++ routes/webRoutes/webRouter.js
@@ -1,57 +1,48 @@
+const usersController = require('../../controllers/users');
 const middleware = require("../../middleware/auth");
-const auth = require("../../controllers/users");
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 
 // Route for register page
-router.get("/register", (req, res) => {
+router.get("/register", async (req, res) => {
   res.render("public/register", {});
 });
 
 // Route for login page
-router.get("/login", (req, res) => {
+router.get("/login", async (req, res) => {
   res.render("public/login", {});
 });
 
 // Route for home page
-router.get("/home", async(req, res) => {
-  res.render("./index",{isAuthenticated: await middleware.isLoggedIn(req), isAdmin: await middleware.isAdmin(req)});
+router.get("/home", middleware.verifyJwt, async (req, res) => {
+  res.render("./index");
 });
 
 // Route for displaying branches
-router.get("/branches", async (req, res) => {
-  res.render("public/branches", {isAuthenticated: await middleware.isLoggedIn(req), isAdmin: await middleware.isAdmin(req)});
+router.get("/branches", middleware.verifyJwt, async (req, res) => {
+  res.render("public/branches");
+});
+
+router.get('/menu', middleware.verifyJwt, async (req, res) => {
+  res.render('./menu');
+})
+
+router.get("/adminPortal", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
+  res.render("./adminPortal.ejs");
 });
 
 router.get("/branchManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
   res.render("./branchMgmt");
 });
 
-router.get("/products/mgmt", async (req, res) => {
+router.get("/products/mgmt", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
   res.render("./productsMgmt");
 });
 
-router.get('/menu', async (req, res) => {
-  res.render('./menu');
-})
-
-router.get("/adminPortal", middleware.verifyJwt, middleware.verifyAdmin, (req, res) => {
-      res.render("./adminPortal.ejs", {isAuthenticated: true, isAdmin: true});
+router.get("/userManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
+  res.render("./userManagement.ejs");
 });
-
-router.get(
-  "/userManagement",
-  middleware.verifyJwt,
-  middleware.verifyAdmin,
-  (req, res) => {
-      res.render("./userManagement.ejs", {
-          isAuthenticated: true,
-          isAdmin: true,
-      });
-  }
-);
-const usersController = require('../../controllers/users');
 
 router.get('/logout', usersController.logout);
 
