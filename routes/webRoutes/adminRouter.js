f3779123366d8e2ab249a4fe210b55a7880ffb8f
EzPzLs <eyalzohar2@gmail.com>
2024-10-19 16:12:34 +0300

    add admin router

---
+++ routes/webRoutes/adminRouter.js
@@ -0,0 +1,26 @@
+const usersController = require("../../controllers/users");
+const middleware = require("../../middleware/auth");
+const express = require("express");
+const adminRouter = express.Router();
+require("dotenv").config();
+
+adminRouter.get("/adminPortal", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
+  res.render("./adminPortal.ejs", { isAdmin: await middleware.isAdmin(req) });
+});
+
+adminRouter.get("/branchManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
+  res.render("./branchMgmt", { isAdmin: await middleware.isAdmin(req) });
+});
+
+adminRouter.get("/productManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
+  res.render("./productMgmt", { isAdmin: await middleware.isAdmin(req) });
+});
+
+adminRouter.get("/userManagement", middleware.verifyJwt, middleware.verifyAdmin, async (req, res) => {
+  res.render("./userManagement.ejs", { isAdmin: await middleware.isAdmin(req) });
+});
+
+
+module.exports = adminRouter;
+
+
